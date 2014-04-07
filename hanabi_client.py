5883088d228f054e17bc103d07aad8aa99fec112
Glikin A <shadow0004@mail.ru>
2014-04-07 16:22:58 +0400

    closed#2

--- hanabi_client.py
+++ hanabi_client.py
@@ -4,21 +4,45 @@ def finish_game(game_result):
     elif game_result is False:
         print('You lose')
 
+def is_int(el):
+    try:
+        int(el)
+        return True
+    except:
+        return False
+
 def move(client_position, players, player_num):
+    commands = ["put", "discard", "support"]
     print("YOUR TURN:")
     if len(client_position.players_hands[player_num]):
         print('  put [card number]')
         print('  discard [card number]')
     if client_position.hints_count:
         print("  support [player's number] [color or value]")
-    client_step = input().split()
-    
-    if client_step[0] == 'support':
-        move = ['support', int(client_step[1]), client_step[2]]
-    elif client_step[0] == 'discard':
-        move = ['discard', int(client_step[1])]
-    elif client_step[0] == 'put':
-        move = ['put', int(client_step[1])]
+    ready = 0
+    my_cards_quantity = len(client_position.players_hands[player_num])
+    while ready == 0:
+        ready = 1
+        client_step = input().split()
+        if (    client_step[0] == 'support' 
+            and len(client_step) == 3
+            and is_int(client_step[1])
+            and client_step[1] < len(players)
+            and int(client_step[2]) in range(1, 6) if is_int(client_step[2]) else
+                client_step[2].lower() in ["blue", "green", "red", "yellow", "white"]    ):
+            move = ['support', int(client_step[1]), client_step[2]]
+        elif (    client_step[0] == 'discard'
+              and len(client_step) == 2
+              and is_int(client_step[1])
+              and int(client_step[1]) in range(my_cards_quantity):
+            move = ['discard', int(client_step[1])]
+        elif (    client_step[0] == 'put'
+              and len(client_step) == 2
+              and is_int(client_step[1])
+              and int(client_step[1]) in range(my_cards_quantity):
+            move = ['put', int(client_step[1])]
+        else:
+            ready = 0
         
     return move
 
